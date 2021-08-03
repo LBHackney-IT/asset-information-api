c0aa8bb3cdc0d9e646cca5f8236c97ec473ee8b0
AtiyyahRawat <atiyyah.rawat@gmail.com>
2021-08-03 12:05:38 +0100

    Disable dynamodb setup until requirements are defined

--- terraform/development/dynamodb.tf
+++ terraform/development/dynamodb.tf
@@ -1,24 +1,25 @@
-resource "aws_dynamodb_table" "assetsinformationapi_dynamodb_table" {
-    name                  = "Assets"
-    billing_mode          = "PROVISIONED"
-    read_capacity         = 10
-    write_capacity        = 10
-    hash_key              = "id"
+# resource "aws_dynamodb_table" "assetinformationapi_dynamodb_table" {
+#     name                  = "Assets"
+#     billing_mode          = "PROVISIONED"
+#     read_capacity         = 10
+#     write_capacity        = 10
+#     hash_key              = "id"
 
-    attribute {
-        name              = "id"
-        type              = "S"
-    }
+#     attribute {
+#         name              = "id"
+#         type              = "S"
+#     }
 
-    tags = {
-        Name              = "assets-information-api-${var.environment_name}"
-        Environment       = var.environment_name
-        terraform-managed = true
-        project_name      = var.project_name
-        backup_policy     = "Dev"
-    }
+#     tags = {
+#         Name              = "asset-information-api-${var.environment_name}"
+#         Environment       = var.environment_name
+#         terraform-managed = true
+#         project_name      = var.project_name
+#         backup_policy     = "Dev"
+#     }
 
-    point_in_time_recovery {
-        enabled           = true
-    }
-}
+#     point_in_time_recovery {
+#         enabled           = true
+#     }
+# }
+# TBD 
