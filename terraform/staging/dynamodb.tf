bb30ff0a8f8be69f5e9e9431f46886a08c1a4880
Charnjit Sohal <charnjit.sohal@news.co.uk>
2021-09-15 17:22:36 +0100

    added CW dashboard

--- terraform/staging/dynamodb.tf
+++ terraform/staging/dynamodb.tf
@@ -1,43 +1,40 @@
 resource "aws_dynamodb_table" "assetinformationapi_dynamodb_table" {
-    name                  = "Assets"
-    billing_mode          = "PROVISIONED"
-    read_capacity         = 10
-    write_capacity        = 10
-    hash_key              = "id"
+  name           = "Assets"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 10
+  write_capacity = 10
+  hash_key       = "id"
 
-    attribute {
-        name              = "id"
-        type              = "S"
-    }
+  attribute {
+    name = "id"
+    type = "S"
+  }
 
-    attribute {
-        name              = "parentAssetIds"
-        type              = "S"
-    }
+  attribute {
+    name = "parentAssetIds"
+    type = "S"
+  }
 
-    attribute {
-        name              = "rootAsset"
-        type              = "S"
-    }
+  attribute {
+    name = "rootAsset"
+    type = "S"
+  }
 
-    global_secondary_index {
-        name              = "AssetParentsAndChilds"
-        hash_key          = "rootAsset"
-        range_key         = "parentAssetIds"
-        write_capacity    = 10
-        read_capacity     = 10
-        projection_type   = "ALL"
-    }
+  global_secondary_index {
+    name            = "AssetParentsAndChilds"
+    hash_key        = "rootAsset"
+    range_key       = "parentAssetIds"
+    write_capacity  = 10
+    read_capacity   = 10
+    projection_type = "ALL"
+  }
 
-    tags = {
-        Name              = "asset-information-api-${var.environment_name}"
-        Environment       = var.environment_name
-        terraform-managed = true
-        project_name      = var.project_name
-        BackupPolicy     = "Stg"
-    }
+  tags = merge(
+    local.default_tags,
+    { BackupPolicy = "Stg" }
+  )
 
-    point_in_time_recovery {
-        enabled           = true
-    }
+  point_in_time_recovery {
+    enabled = true
+  }
 }
