00208356d9e9839dcf4dd69ad860f3bff1959b89
Charnjit Sohal <charnjit.sohal@news.co.uk>
2021-09-16 11:25:30 +0100

    update dashboards

--- terraform/development/main.tf
+++ terraform/development/main.tf
@@ -44,8 +44,9 @@ terraform {
 }
 
 module "asset_information_api_cloudwatch_dashboard" {
-  source              = "github.com/LBHackney-IT/aws-hackney-common-terraform.git//modules/cloudwatch/dashboards/api-dashboard"
+  source              = "github.com/LBHackney-IT/aws-hackney-common-terraform.git?ref=master//modules/cloudwatch/dashboards/api-dashboard"
   environment_name    = var.environment_name
   api_name            = "asset-information-api"
   dynamodb_table_name = aws_dynamodb_table.assetinformationapi_dynamodb_table.name
+  include_sns_widget  = false
 }
