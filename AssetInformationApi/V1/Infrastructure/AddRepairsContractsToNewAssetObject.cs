71477ce0beac160bb91a1bca85d724f3ff37cec0
ange47rm <ange47rm@hotmail.com>
2023-08-29 13:04:44 +0100

    Added comment

--- AssetInformationApi/V1/Infrastructure/AddRepairsContractsToNewAssetObject.cs
+++ AssetInformationApi/V1/Infrastructure/AddRepairsContractsToNewAssetObject.cs
@@ -2,6 +2,7 @@ using System;
 
 namespace AssetInformationApi.V1.Infrastructure
 {
+    // The same class is also defined in Repairs Listener to ensure the information sent and received is in the same format
     public class AddRepairsContractsToNewAssetObject
     {
         public Guid EntityId { get; set; }
